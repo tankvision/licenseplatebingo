0e154a30416d27a83414aae86643c398ebaf3fa1
tankvision <55211344+tankvision@users.noreply.github.com>
2026-07-29 21:29:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    BUMP THIS STRING EVERY TIME YOU CHANGE index.html.
    Nothing else in the deploy matters as much as this line.
    ───────────────────────────────────────────────────────────── */
-const VERSION = 'lpb-2026-07-20a';
+const VERSION = 'lpb-2026-07-27c';
 
 const FONTS = VERSION + '-fonts';
 const SHELL = [
